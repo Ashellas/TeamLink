a764f10230aed03ce37c4a6bfbfe8ed2803ed64d
AtakTalay <40243456+AtakTalay@users.noreply.github.com>
2021-04-18 17:48:35 +0300

    merge with Talay branch

--- src/Main.java
+++ src/Main.java
@@ -7,41 +7,25 @@ import javafx.scene.Scene;
 import javafx.scene.transform.Scale;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
+
 import javafx.stage.StageStyle;
 
 import java.awt.*;
-=======
+
 import javafx.stage.StageStyle;
 import java.awt.*;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
->>>>>>> Talay
+
 
 public class Main extends Application {
 //aasd
     @Override
     public void start(Stage primaryStage) throws Exception{
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-        Parent root = FXMLLoader.load(getClass().getResource("views/MainTemplate2.fxml"));
-=======
-<<<<<<< Updated upstream
-        Parent root = FXMLLoader.load(getClass().getResource("views/MainTemplate.fxml"));
-        primaryStage.setTitle("Hello World");
-=======
         Parent root = FXMLLoader.load(getClass().getResource("views/DemoScene.fxml"));
->>>>>>> Stashed changes
         primaryStage.setTitle("TeamLink");
->>>>>>> Stashed changes
-=======
 
-        Parent root = FXMLLoader.load(getClass().getResource("views/DemoScene.fxml"));
-        primaryStage.setTitle("TeamLink");
->>>>>>> Talay
         primaryStage.setScene(new Scene(root));
         primaryStage.setMaximized(true);
         primaryStage.setMinHeight(700);
