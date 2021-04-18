1dada158c6879c453c94ab758d187999f4b18084
AtakTalay <40243456+AtakTalay@users.noreply.github.com>
2021-04-18 17:49:10 +0300

    problem solved

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,10 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.awt.*;
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 import javafx.stage.StageStyle;
 import java.awt.*;
 import java.io.IOException;
@@ -23,9 +26,15 @@ public class Main extends Application {
 //aasd
     @Override
     public void start(Stage primaryStage) throws Exception{
+<<<<<<< Updated upstream
         Parent root = FXMLLoader.load(getClass().getResource("views/DemoScene.fxml"));
         primaryStage.setTitle("TeamLink");
 
+=======
+
+        Parent root = FXMLLoader.load(getClass().getResource("views/DemoScene.fxml"));
+        primaryStage.setTitle("TeamLink");
+>>>>>>> Stashed changes
         primaryStage.setScene(new Scene(root));
         primaryStage.setMaximized(true);
         primaryStage.setMinHeight(700);
