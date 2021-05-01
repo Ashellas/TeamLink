103494f6f5fc3180fb4c734be3d4beabe4554e84
AtakTalay <40243456+AtakTalay@users.noreply.github.com>
2021-05-01 19:54:13 +0300

    resources and views updated

--- src/models/AppManager.java
+++ src/models/AppManager.java
@@ -14,19 +14,6 @@ import java.net.URL;
 import java.util.Date;
 
 public class AppManager {
-    public static void changeScene(URL fxmlfile, ActionEvent event, UserSession userSession) throws IOException {
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(fxmlfile);
-        loader.load();
-        InitializeData c2 = loader.getController();
-        c2.initData(userSession);
-        Parent p = loader.getRoot();
-        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        Scene scene = new Scene(p, ((Node) event.getSource()).getScene().getWidth(), ((Node) event.getSource()).getScene().getHeight());
-        scene.getStylesheets().add(userSession.getStyleSheet());
-        stage.setScene(scene);
-        stage.show();
-    }
 
     public static String getLastSyncText(Date lastSync){
         long differenceInMinutes = (new Date().getTime() - lastSync.getTime()) / 1000 / 60;
@@ -54,4 +41,40 @@ public class AppManager {
         fadeTransition.setToValue(1);
         fadeTransition.play();
     }
+
+    public static void changeScene(URL fxmlfile, ActionEvent event, UserSession userSession) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(fxmlfile);
+        loader.load();
+        InitializeData c2 = loader.getController();
+        c2.initData(userSession);
+        Parent p = loader.getRoot();
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(p, ((Node) event.getSource()).getScene().getWidth(), ((Node) event.getSource()).getScene().getHeight());
+        scene.getStylesheets().add(userSession.getStyleSheet());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    public static void openScene(URL fxmlfile, ActionEvent event, UserSession userSession, Team team) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(fxmlfile);
+        loader.load();
+        TeamEditScreenController c2 = loader.getController();
+        c2.initData(userSession);
+        c2.getInformation(team);
+        Parent p = loader.getRoot();
+        Stage teamEdit = new Stage();
+        Scene scene = new Scene(p);
+        teamEdit.initModality(Modality.APPLICATION_MODAL);
+        teamEdit.initStyle(StageStyle.UNDECORATED);
+        scene.getStylesheets().add(userSession.getStyleSheet());
+        teamEdit.setScene(scene);
+        teamEdit.setWidth(1100);
+        teamEdit.setHeight(700);
+        teamEdit.setResizable(false);
+        teamEdit.show();
+    }
+
 }
